8c944ebc2723ad8ad13f84232ef6a9314411c2f0
Steven Nguyen <steven38016@gmail.com>
2016-11-19 22:55:20 -0600

    Delete dallas_service.py

--- app/services/dallas_service.py
+++
@@ -1,8 +0,0 @@
-from app.models import PoliceCall
-
-def create_police_call(street_number, street_name, description, created):
-    return PoliceCall.objects.create(
-    	street_number = street_number,
-    	street_name = street_name,
-    	description = description,
-    	created = created)
