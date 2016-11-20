55cf01112e67ba8efd9ddf53b2ae1fc4d7cbaa3c
steven <steven@stevennguyen.io>
2016-11-19 22:38:14 -0600

    reupload api

--- app/dallasOpenData.py
+++ app/dallasOpenData.py
@@ -1,22 +1,17 @@
 import requests
 import json
 
+from app.services.dallas_service import dallas_service
+
 url = "https://www.dallasopendata.com/resource/are8-xahz.json"
 
 response = requests.get(url)
 if response.status_code == 200:
 	data = response.json()
 	data_index = 0
-	#json_data = json.loads(str(data))
-	#data = json.loads(response.text)
-	#for d in data:
-	#	print d.get("block")
 	while True:
 		try:
-			print data[data_index]["block"]
-			print data[data_index]["location"]
-			print data[data_index]["date_time"]
-			print data[data_index]["nature_of_call"] 
+			create_police_call(data[data_index]["block"], data[data_index]["location"], data[data_index]["nature_of_call"], data[data_index]["date_time"])
 		except KeyError:
 			data_index += 1
 		data_index += 1
